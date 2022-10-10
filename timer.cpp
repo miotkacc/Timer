cad52ffb1c033ff8184c258452c4fb54335cee52
michal.miotk <michal.miotk@accenture.com>
2022-10-10 10:32:36 +0200

    added get_elapsed_time

--- timer.cpp
+++ timer.cpp
@@ -3,4 +3,14 @@
 
 void Timer::start(){
         std::cout<<"start timer"<<std::endl;
+        //startTime = ;
+        startTime = std::chrono::system_clock::now();
+}
+void Timer::stop(){
+        std::cout<<"start timer"<<std::endl;
+        stopTime = std::chrono::system_clock::now();
+}
+
+std::chrono::milliseconds Timer::get_elapsed_time(){
+    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime);
 }
--- timer.hpp
+++ timer.hpp
@@ -1,3 +1,9 @@
+#include <chrono>
+
 struct Timer{
     void start();
+    void stop();
+    std::chrono::milliseconds get_elapsed_time();
+    std::chrono::time_point<std::chrono::system_clock> startTime;
+    std::chrono::time_point<std::chrono::system_clock> stopTime;
 };
